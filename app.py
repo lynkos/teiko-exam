763f9eb0d989146202a344825086b892843704e3
Kiran Brahmatewari <kiwi2mii@gmail.com>
2026-02-15 19:06:03 -0500

    Improve dashboard style

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from subset_analysis import DATA_FRAME_PROJECTS, DATA_FRAME_RESPONDERS, DATA_FRA
 
 HEADER_SIZE = 25
 FONT_SIZE = 18
+FONT_FAMILY = "Arial"
 SMALL_TABLE_WIDTH = "30rem"
 THEME = "themeBalham"
 
@@ -15,9 +16,9 @@ app = Dash(__name__, title = "Teiko Exam - Data Analysis")
 
 app.layout = [
     html.H1(children = "Part 2: Initial Analysis - Data Overview",
-            style = { 'textAlign': 'center', 'fontSize': HEADER_SIZE }),
+            style = { "textAlign": "center", "fontSize": HEADER_SIZE, "fontFamily": FONT_FAMILY }),
     html.H2(children = "Summary Table",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+            style = { "textAlign": "center", "fontSize": HEADER_SIZE, "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = DATA_FRAME_SUMMARY.to_dict("records"),
         columnSize = "responsiveSizeToFit",
@@ -26,23 +27,23 @@ app.layout = [
     ),
 
     html.H1(children = "Part 3: Statistical Analysis",
-            style = { 'textAlign': 'center', 'fontSize': HEADER_SIZE }),
+            style = { "textAlign": "center", "fontSize": HEADER_SIZE, "fontFamily": FONT_FAMILY }),
     
     # Boxplot displaying the relative frequencies of responders vs. non-responders for each cell population
     dcc.Graph(figure = px.box(RESP_FREQ_DF,
                               x = "population",
                               y = "percentage",
                               color = "response",
-                              labels = { "population": "Cell Population", "percentage": "Relative Frequency (%)", "response": "Response Status" },
+                              labels = { "population": "Cell Population", "percentage": "Relative Frequency (%)", "response": "Response" },
                               title = "Responders vs. Non-Responders",
                               subtitle = "Relative frequencies of responders vs. non-responders for each cell population"
                             )
               ),
     
     html.H2(children = "Differences in relative frequencies between responders vs. non-responders for each cell population",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     html.H3(children = "This data is for all samples with responses",
-            style = { 'textAlign': 'center', 'fontSize': (FONT_SIZE * 0.9) }),
+            style = { "textAlign": "center", "fontSize": (FONT_SIZE * 0.9), "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = COMPARISON.to_dict("records"),
         dashGridOptions = { "domLayout": "autoHeight", "theme": THEME },
@@ -53,16 +54,16 @@ app.layout = [
                               x = "population",
                               y = "percentage",
                               color = "response",
-                              labels = { "population": "Cell Population", "percentage": "Relative Frequency (%)", "response": "Response Status" },
+                              labels = { "population": "Cell Population", "percentage": "Relative Frequency (%)", "response": "Response" },
                               title = "Filtered Responders vs. Non-Responders",
                               subtitle = "Relative frequencies of melanoma patients receiving miraclib who respond vs. non-responders for each cell population that includes PBMC samples"
                             )
               ),
 
     html.H2(children = "Differences in relative frequencies between responders vs. non-responders for each cell population",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     html.H3(children = "This data is filtered to only include melanoma patients receiving miraclib with PBMC samples",
-            style = { 'textAlign': 'center', 'fontSize': (FONT_SIZE * 0.9) }),
+            style = { "textAlign": "center", "fontSize": (FONT_SIZE * 0.9), "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = COMPARISON_FILTER.to_dict("records"),
         dashGridOptions = { "domLayout": "autoHeight", "theme": THEME },
@@ -70,20 +71,20 @@ app.layout = [
     ),
 
     html.H1(children = "Part 4: Data Subset Analysis",
-            style = { 'textAlign': 'center', 'fontSize': HEADER_SIZE }),
+            style = { "textAlign": "center", "fontSize": HEADER_SIZE, "fontFamily": FONT_FAMILY }),
 
     html.H2(children = "1. All melanoma PBMC samples at baseline from patients treated with miraclib",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = DATA_FRAME_FILTER.to_dict("records"),
         columnSize = "responsiveSizeToFit",
         style = { "width": SMALL_TABLE_WIDTH, "margin": "0 auto" },
-        columnDefs = [ { "field": i } for i in DATA_FRAME_FILTER.columns ],
+        columnDefs = [ { "field": i, "filter": True } for i in DATA_FRAME_FILTER.columns ],
         dashGridOptions = { "theme": THEME },
     ),
 
     html.H2(children = "2. How many samples from each project",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = DATA_FRAME_PROJECTS.to_dict("records"),
         dashGridOptions = { "domLayout": "autoHeight", "theme": THEME },
@@ -92,8 +93,8 @@ app.layout = [
         columnDefs = [ { "field": i } for i in DATA_FRAME_PROJECTS.columns ]
     ),
 
-    html.H2(children = "3. How many subjects were responders/non-responders",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+    html.H2(children = "3. How many subjects were Responders (yes) vs Non-Responders (no)",
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = DATA_FRAME_RESPONDERS.to_dict("records"),
         dashGridOptions = { "domLayout": "autoHeight", "theme": THEME },
@@ -102,8 +103,8 @@ app.layout = [
         columnDefs = [ { "field": i } for i in DATA_FRAME_RESPONDERS.columns ]
     ),
 
-    html.H2(children = "4. How many subjects were males/females",
-            style = { 'textAlign': 'center', 'fontSize': FONT_SIZE }),
+    html.H2(children = "4. How many subjects were Males (M) vs. Females (F)",
+            style = { "textAlign": "center", "fontSize": FONT_SIZE, "fontFamily": FONT_FAMILY }),
     dag.AgGrid(
         rowData = DATA_FRAME_SEXES.to_dict("records"),
         dashGridOptions = { "domLayout": "autoHeight", "theme": THEME },
